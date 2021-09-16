493843fe1bb01df2ee4dab2213b00d3b3b6715f8
m266 <herbrand@mailbox.org>
2021-09-16 10:04:25 +0200

    Update 0.9.6

--- header_home.php
+++ header_home.php
@@ -9,57 +9,70 @@
  * @package Blocksy
  */
 
-?>
+;?>
 
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
 <head>
-	<?php do_action('blocksy:head:start') ?>
+    <?php do_action('blocksy:head:start');?>
 
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, viewport-fit=cover">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, viewport-fit=cover">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
-	<?php do_action('blocksy:head:end') ?>
+    <?php wp_head();?>
+    <?php do_action('blocksy:head:end');?>
 </head>
 
-<body <?php body_class(); ?> <?php echo blocksy_body_attr() ?>>
+<body <?php body_class();?> <?php echo blocksy_body_attr(); ?>>
 
 <a class="skip-link show-on-focus" href="#main">
-	<?php echo __('Skip to content', 'blocksy'); ?>
+    <?php echo __('Skip to content', 'blocksy'); ?>
 </a>
 
 <?php
-	if (function_exists('wp_body_open')) {
-		wp_body_open();
-	}
+if (function_exists('wp_body_open')) {
+    wp_body_open();
+}
 ?>
 
 <div id="main-container">
-	<?php
-		do_action('blocksy:header:before');
+    <?php
+do_action('blocksy:header:before');
 
-		blocksy_output_header();
+blocksy_output_header();
 
-		do_action('blocksy:header:after');
-		do_action('blocksy:content:before');
-	?>
-
-	<main <?php echo blocksy_main_attr() ?>>
-<?php
-// Headerbild für Startseite
-if ( is_front_page() ) {
+do_action('blocksy:header:after');
+do_action('blocksy:content:before');
 ?>
+
+    <main <?php echo blocksy_main_attr(); ?>>
+<?php
+/* Headerbild für die Startseite
+ * Eigenes Headerbild in die Mediathek hochladen
+ * Bildlink im Format "/wp-content/..." als Wert im Benutzerdefiniertes Feld eingefügen
+ * Ohne Eingabe eines Wertes wird das Standardbild dargestellt
+ */
+$sbr_headerbild = get_post_meta($post->ID, 'headerbild', true);
+if (is_front_page()) {
+    ?>
 <div style="text-align: center;">
-<img class="size-full" src="/wp-content/themes/sbr-theme-2/images/sbr_banner.jpg" alt="" width="2335" height="329"/>
+<img class="size-full" src="
+<?php
+if ($sbr_headerbild == true) {
+        echo $sbr_headerbild;
+    } else {
+        echo "/wp-content/themes/sbr-theme-2/images/sbr_banner.jpg"; /* Default-Bild */
+    }
+    ?>"
+alt="" width="2335" height="329"/>
 <br>
 </div>
 <?php
 ;
 }
 ?>
-		<?php
-			do_action('blocksy:content:top');
-			blocksy_before_current_template();
-		?>
+        <?php
+do_action('blocksy:content:top');
+blocksy_before_current_template();
+?>
