2d7466d6645607008601d3031f7e71ae2ca0f2f5
m266 <herbrand@mailbox.org>
2021-10-09 11:22:55 +0200

    Update 0.9.6.3.3

--- functions.php
+++ functions.php
@@ -3,18 +3,12 @@ if (!defined('WP_DEBUG')) {
     die('Direct access forbidden.');
 }
 
-// Recommended way to include parent theme styles.
-//  (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
-//
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles()
-{
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
+// Child-Theme style.css nach dem Parent-Theme style.css laden
+function sbr_enqueue_styles() {
+wp_enqueue_style( 'child-main', get_stylesheet_directory_uri
+()."/style.css" );
 }
+add_action( 'wp_enqueue_scripts', 'sbr_enqueue_styles',999);
 
 // Anpassungen fuer SBR-Websites
 //////////////////////////////////////////////////////////////////////////////////////////
