dee0c48335d0c0cbc2259b753b672ef464157329
m266 <herbrand@mailbox.org>
2022-03-10 10:37:46 +0100

    Update 0.9.7.3

--- header_home.php
+++ header_home.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The header for start page
+ * The header for our theme
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
@@ -9,44 +9,46 @@
  * @package Blocksy
  */
 
-;?>
-
-<!doctype html>
-<html <?php language_attributes();?>>
+?><!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
-    <?php do_action('blocksy:head:start');?>
+    <?php do_action('blocksy:head:start') ?>
 
-    <meta charset="<?php bloginfo('charset');?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, viewport-fit=cover">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
-    <?php wp_head();?>
-    <?php do_action('blocksy:head:end');?>
+    <?php wp_head(); ?>
+    <?php do_action('blocksy:head:end') ?>
 </head>
 
-<body <?php body_class();?> <?php echo blocksy_body_attr(); ?>>
+<body <?php body_class(); ?> <?php echo blocksy_body_attr() ?>>
 
-<a class="skip-link show-on-focus" href="#main">
+<a class="skip-link show-on-focus" href="<?php echo apply_filters('blocksy:head:skip-to-content:href', '#main') ?>">
     <?php echo __('Skip to content', 'blocksy'); ?>
 </a>
 
 <?php
-if (function_exists('wp_body_open')) {
-    wp_body_open();
-}
+    ob_start();
+    blocksy_output_header();
+    $global_header = ob_get_clean();
+
+    if (function_exists('wp_body_open')) {
+        wp_body_open();
+    }
 ?>
 
 <div id="main-container">
     <?php
-do_action('blocksy:header:before');
+        do_action('blocksy:header:before');
 
-blocksy_output_header();
+        echo $global_header;
 
-do_action('blocksy:header:after');
-do_action('blocksy:content:before');
-?>
+        do_action('blocksy:header:after');
+        do_action('blocksy:content:before');
+    ?>
 
-    <main <?php echo blocksy_main_attr(); ?>>
+    <main <?php echo blocksy_main_attr() ?>>
 <?php
 /* Headerbild für die Startseite
  * Eigenes Headerbild in die Mediathek hochladen
@@ -65,7 +67,7 @@ if ($sbr_headerbild == true) {
         echo "/wp-content/themes/sbr-theme-2/images/sbr_banner.jpg"; /* Default-Bild */
     }
     ?>"
-alt="" width="2335" height="329"/>
+alt="" width="1920" height="690"/>
 <br>
 </div>
 <?php
