d6b602652dca6cb0b830eadf73c377243243b164
m266 <herbrand@mailbox.org>
2024-05-20 15:14:08 +0200

    Update 20.05.2024

--- check_plugins_active.php
+++ check_plugins_active.php
@@ -3,14 +3,14 @@
 Stand: 25.01.2024
 Check, ob Plugins aktiv/inaktiv sind
 */
-// Plugin "WP H-Happyforms Tools" aktiv?
+// Plugin "Comment Blacklist Updater" aktiv?
 if (!function_exists('is_plugin_active')) {
 require_once ABSPATH . '/wp-admin/includes/plugin.php';
 }
-if (is_plugin_active('wp-h-happyforms-tools/wphhft.php')) {
+if (is_plugin_active('comment-blacklist-updater/comment-blacklist-updater.php')) {
 function wphhft_notice() { ?>
 <div class="notice notice-error">
-<p><?php _e('Bitte das Plugin <b>"WP H-Happyforms Tools"</b></a> deaktivieren und l&ouml;schen!');?></p>
+<p><?php _e('Bitte das Plugin <b>"Comment Blacklist Updater"</b></a> deaktivieren und l&ouml;schen!');?></p>
 
 </div>
 <?php
